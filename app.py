050ec9d412aa9b6bdd2ee6e0131dd257df15df2a
temp19091 <doragan079@gmail.com>
2024-06-15 14:08:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 import os
 
-import streamlit as st
-
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#MainMenu {
-    visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 # Your app code goes here
 
